c87c094c8b1aad3bf561767a5930ed40af2006b2
Rogerio Pereira <hi@rogeriopereira.dev>
2021-08-25 23:23:56 -0400

    Aula: 03 - Inserir Manualmente Resources e Permissions no Laravel com Seeders

---
+++ database/seeders/ResourceSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Resource;
+use Illuminate\Database\Seeder;
+
+class ResourceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $category = Resource::create(['name' => 'Categoria']);
+        $category->permissions()->create(['name' => 'visualizar_categorias']);
+        $category->permissions()->create(['name' => 'visualizar_categoria']);
+        // $category->permissions()->create(['name' => 'criar_categoria']);
+        $category->permissions()->create(['name' => 'editar_categoria']);
+        $category->permissions()->create(['name' => 'deletar_categoria']);
+
+        $company = Resource::create(['name' => 'Empresas']);
+        $company->permissions()->create(['name' => 'visualizar_empresas']);
+        $company->permissions()->create(['name' => 'visualizar_empresa']);
+        // $company->permissions()->create(['name' => 'criar_empresa']);
+        $company->permissions()->create(['name' => 'editar_empresa']);
+        $company->permissions()->create(['name' => 'deletar_empresa']);
+    }
+}
